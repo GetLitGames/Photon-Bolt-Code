cac2ea1ed3521f1f861b27909eb53f5e24397ce4
Michael Urvan <michaelurvan@gmail.com>
2020-07-25 16:20:16 -0400

    GameConfig for dedicated server options or just INI file options for your games

---
+++ Misc/GameConfig.cs
@@ -0,0 +1,42 @@
+using SharpConfig;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GameConfig : Singleton<GameConfig>
+{
+	public string configFileName = "gameconfig.cfg";
+	[System.NonSerialized]
+	public Configuration cfg = new Configuration();
+
+	void Start()
+	{
+		if (!File.Exists (configFileName))
+		{
+			Debug.Log ("Setting up a default config since no file was found!");
+			SetupCleanCFG ();
+			SaveConfig ();
+		}
+
+		// Load the configuration.
+		cfg = Configuration.LoadFromFile(configFileName);
+	}
+
+	public void SaveConfig()
+	{
+		Debug.Log ("Saving Client config...");
+
+		// Save the configuration.
+		cfg.SaveToFile (configFileName);
+	}
+
+	private void SetupCleanCFG()
+	{
+		ServerPort = 42424;
+		ServerName = "Grasslands World";
+	}
+
+	public int ServerPort { get { return cfg["Server"]["Port"].IntValue; } set { cfg["Server"]["Port"].IntValue = value; } }
+	public string ServerName { get { return cfg["Server"]["Name"].StringValue; } set { cfg["Server"]["Name"].StringValue = value; } }
+}
